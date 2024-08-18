edc6ef172297578182996bbd6c1897d3da553d73
Jack Kitto <jack.kitto@gmail.com>
2024-08-18 18:54:06 +1000

    feat: Start bubbletea app

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"secretly-cli/internal/models"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	fmt.Println("Welcome to secretly-cli")
-	fmt.Println("Feature 1")
-	fmt.Println("Fix 1")
-	fmt.Println("Breaking Feature 1")
+	p := tea.NewProgram(models.MainModel_New())
+
+	if err := p.Start(); err != nil {
+		fmt.Printf("Error: %v", err)
+	}
 }
