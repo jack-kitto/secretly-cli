37b872a78f2d12d907922c05798b7e907c267385
Jack Kitto <jack.kitto@gmail.com>
2024-08-21 19:51:47 +1000

    feat: add cobra

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"secretly-cli/internal/models"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	p := tea.NewProgram(models.MainModel_New())
+var rootCmd = &cobra.Command{
+	Use:   "secretly-cli",
+	Short: "Securely share environment variables with your team.",
+	Long: `Secretly is a powerful tool for securely managing and sharing environment variables with your team. 
+       Integrated with the Secretly website at https://secretly.kitto.sh, it ensures your secrets are handled safely and easily, 
+       so you can focus on building your projects without the worry of exposing sensitive information. 
+       Complete documentation is available at https://secretly.kitto.sh/docs.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		p := tea.NewProgram(models.MainModel_New())
 
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Error: %v", err)
+		if _, err := p.Run(); err != nil {
+			fmt.Printf("Error: %v", err)
+		}
+	},
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/charmbracelet/bubbletea v0.27.0
 	github.com/charmbracelet/lipgloss v0.12.1
 	github.com/jack-kitto/secretly-sdk v1.2.0
+	github.com/spf13/cobra v1.8.1
 )
 
 require (
@@ -18,6 +19,7 @@ require (
 	github.com/charmbracelet/x/term v0.1.1 // indirect
 	github.com/charmbracelet/x/windows v0.1.0 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/lucsky/cuid v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -27,6 +29,7 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
